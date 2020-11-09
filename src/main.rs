08f4988fed287b66da6e7084c91bbb05b92c2aeb
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2020-11-08 22:03:03 -0500

    parse url

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,63 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use dotenv::dotenv;
-use reqwest::header::USER_AGENT;
+use regex::Regex;
+use reqwest::header::{ACCEPT, USER_AGENT};
 use serde::Deserialize;
+
+use std::process::{Command, Stdio};
 
 #[macro_use]
 extern crate dotenv_codegen;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
-
     dotenv().ok();
 
+    let input =
+        r#"
+"<https://api.github.com/user/repos?page=1&per_page=100>; rel=\"prev\", <https://api.github.com/user/repos?page=1&per_page=100>; rel=\"last\", <https://api.github.com/user/repos?page=1&per_page=100>; rel=\"first\""
+        "#
+        .trim();
+
+    let link_pattern = Regex::new(r#"<(.+)>; rel=\\"(.+)\\""#).unwrap();
+    let caps = link_pattern.captures(input)
+        .with_context(|| format!("couldn't parse page from input {}", input))?;
+
+    get_repos();
+    // call_fzf();
+
+    Ok(())
+}
+
+fn parse_links(link_raw: &str) -> Result<()> {
+    // <https://api.github.com/user/repos?page=1&per_page=100>;
+    // rel=\"prev\", <https://api.github.com/user/repos?page=1&per_page=100>; rel=\"last\", <https://api.github.com/user/repos?page=1&per_page=100>; rel=\"first\"
+    Ok(())
+}
+
+fn call_fzf() -> Result<()> {
+    // Command::new("fzf").stdin(Stdio::piped)?;
+
+    Ok(())
+}
+
+fn get_repos() -> Result<()> {
     let client = reqwest::blocking::Client::new();
-    let body = client
+    let response = client
         .get("https://api.github.com/user/repos")
         .header(USER_AGENT, "rperryng")
+        .header(ACCEPT, "application/vnd.github.v3+json")
         .basic_auth("rperryng", Some(dotenv!("GH_ACCESS_TOKEN")))
         .query(&[
-            ("page", "1"),
-            ("per_page", "100")
+            ("page", "7"),
+            ("per_page", "100") // 100
         ])
-        .send()?
-        .text()?;
+        .send()?;
 
-    println!("got response: {:#?}", body);
+    if let Some(link) = response.headers().get("Link") {
+        println!("got headers: {:#?}", link);
+    } else {
+        println!("wat, no 'Link' header");
+    }
 
     Ok(())
 }
