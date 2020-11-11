862734eb53309cc912b01ed021bf329382462885
Ryan Perry-Nguyen <ryanperrynguyen@gmail.com>
2020-11-10 22:10:12 -0500

    convert to tokio

--- src/github.rs
+++ src/github.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, bail, Context, Result};
 use log::info;
 use regex::Regex;
-use reqwest::blocking::{Client, Response};
+use reqwest::{Client, Response};
 use reqwest::header::{ACCEPT, USER_AGENT};
 use serde::Deserialize;
 use url::Url;
@@ -13,7 +13,7 @@ const GH_USER_NAME: &str = dotenv!("GH_USER_NAME");
 
 lazy_static! {
     static ref CLIENT: Client = Client::new();
-    static ref LINK_PATTERN: Regex = Regex::new(r#"<.+\bpage\b=(\d+).+>; rel="last""#).unwrap();
+    static ref LINK_PATTERN: Regex = Regex::new(r#"<.+\bpage\b=(\d+).+>;\s*rel="last""#).unwrap();
 }
 
 #[derive(Deserialize, Debug)]
@@ -27,8 +27,8 @@ struct Link {
     link_type: String,
 }
 
-pub fn get_repo_names() -> Result<Vec<String>> {
-    let response = request()?;
+pub async fn get_repo_names() -> Result<Vec<String>> {
+    let response = request().await?;
 
     let num_pages = response
         .headers()
@@ -39,7 +39,7 @@ pub fn get_repo_names() -> Result<Vec<String>> {
 
     info!("num_pages: {}", num_pages);
 
-    parse_response(response)
+    parse_response(response).await
 }
 
 fn parse_num_pages_from_link_header(link: &str) -> Result<i32> {
@@ -56,9 +56,10 @@ fn parse_num_pages_from_link_header(link: &str) -> Result<i32> {
         .map_err(|e| anyhow!(e))
 }
 
-fn parse_response(response: Response) -> Result<Vec<String>> {
+async fn parse_response(response: Response) -> Result<Vec<String>> {
     let repo_names = response
         .json::<Vec<Repo>>()
+        .await
         .context("Failed to deserialize github response")?
         .iter()
         .map(|repo| repo.full_name.to_string())
@@ -67,7 +68,7 @@ fn parse_response(response: Response) -> Result<Vec<String>> {
     Ok(repo_names)
 }
 
-fn request() -> Result<Response> {
+async fn request() -> Result<Response> {
     CLIENT
         .get(API_URL)
         .header(USER_AGENT, GH_USER_NAME)
@@ -78,5 +79,6 @@ fn request() -> Result<Response> {
             ("per_page", "100"), // 100
         ])
         .send()
-        .context(format!("Failed to send request to GitHub"))
+        .await
+        .map_err(|e| anyhow!(e))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use dotenv::dotenv;
 use log::info;
-use regex::Regex;
-use reqwest::header::{ACCEPT, USER_AGENT};
-
-use std::process::{Command, Stdio};
 
 mod github;
 
@@ -14,29 +10,14 @@ extern crate dotenv_codegen;
 #[macro_use]
 extern crate lazy_static;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     dotenv().ok();
     env_logger::init();
 
-    // let link_pattern = Regex::new(r#"<(.+)>; rel=\\"(.+)\\""#).unwrap();
-    // let caps = link_pattern.captures(input)
-    //     .with_context(|| format!("couldn't parse page from input {}", input))?;
-
-    let repo_names = github::get_repo_names()?;
+    let repo_names = github::get_repo_names().await?;
 
     info!("repo_names: {:?}", repo_names);
-
-    Ok(())
-}
-
-fn parse_links(link_raw: &str) -> Result<()> {
-    // <https://api.github.com/user/repos?page=1&per_page=100>;
-    // rel=\"prev\", <https://api.github.com/user/repos?page=1&per_page=100>; rel=\"last\", <https://api.github.com/user/repos?page=1&per_page=100>; rel=\"first\"
-    Ok(())
-}
-
-fn call_fzf() -> Result<()> {
-    // Command::new("fzf").stdin(Stdio::piped)?;
 
     Ok(())
 }
